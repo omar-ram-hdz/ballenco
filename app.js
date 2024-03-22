bf85d31345da1e1c9f9c74ace0afc0cce865d9ee
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-03-21 22:57:49 -0600

    Add engine, fix errors relationed with app,  server, 404 page

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 import express, { json } from "express";
 import { corsMiddleware } from "./middlewares/cors.js";
 import { createRouter } from "./routes/main.js";
+import consolidate from "consolidate";
 import path from "path";
 
 export const createApp = () => {
   const app = express();
   const publicDirection = express.static("./web/public");
-
+  const cons = consolidate;
+  app.engine("html", cons.swig);
+  app.set("view engine", "html");
+  app.set("views", "./web/views");
   app.use(json());
   app.use(corsMiddleware());
   app.disable("x-powered-by");
--- routes/main.js
+++ routes/main.js
@@ -5,15 +5,7 @@ export const createRouter = () => {
   const mainRouter = Router();
 
   mainRouter.use((req, res) => {
-    fs.readFile("index.html", "utf8", (err, data) => {
-      if (err) {
-        res.writeHead(500, { "Content-Type": "text/plain" });
-        res.end("Error interno del servidor");
-        return;
-      }
-      res.writeHead(200, { "Content-Type": "text/html" });
-      res.end(data);
-    });
+    res.render("404");
   });
 
   return mainRouter;
