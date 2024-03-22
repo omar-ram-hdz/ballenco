623ba1188325f8bb641d2f341a76985ad9d12b28
Omar Ramirez Hernandez <omarramhdz16@gmail.com>
2024-03-21 22:00:37 -0600

    Fix errors on app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 import express, { json } from "express";
-import { corsMiddleware } from "./middlewares/cors";
+import { corsMiddleware } from "./middlewares/cors.js";
 import path from "path";
 
-const app = express();
-const publicDirection = express.static(path.join(__dirname, "web", "public"));
+export const createApp = () => {
+  const app = express();
+  const publicDirection = express.static(path.join(__dirname, "web", "public"));
 
-app.use(json());
-app.use(corsMiddleware());
-app.disable("x-powered-by");
-app.use(publicDirection);
-
-export default app;
+  app.use(json());
+  app.use(corsMiddleware());
+  app.disable("x-powered-by");
+  app.use(publicDirection);
+  return app;
+};
